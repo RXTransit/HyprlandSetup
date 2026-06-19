f72da8b19eb8fb6346d2e59f6d662666eaf65352
Michael Scott <michael.rex.scott@gmail.com>
2026-06-19 21:30:57 +0800

    master

---
+++ README.md
@@ -0,0 +1,2 @@
+These are my Hyprland Dotfiles
+Waybar is a fork of https://github.com/sejjy/mechabar.git
--- azotebg-hyprland.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-pkill swaybg
-swaybg -i "/home/michael-scott/Documents/Wallpapers/Cressida.jpg" -m fill &
