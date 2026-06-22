4953d31c26decb62f16978cc0498db867749fe16
Michael Scott <michael.rex.scott@gmail.com>
2026-06-23 03:28:50 +0800

    fixed up the hyprland.conf with some better comments

--- .local/share/scripts/multimonwallpapers.sh
+++ .local/share/scripts/multimonwallpapers.sh
@@ -1,1 +1,3 @@
-swaybg -o HDMI-A-1 -i /usr/share/backgrounds/river-city.jpg -m fill -o HDMI-A-2 -i /usr/share/backgrounds/river-city.jpg -m fill
+#Replace the monitor names with your ones + change the wallpaper paths if neccessary, and then uncomment below
+
+#swaybg -o HDMI-A-1 -i ~/.local/share/backgrounds/river-city.jpg -m fill -o HDMI-A-2 -i ~/.local/share/backgrounds/river-city.jpg -m fill
